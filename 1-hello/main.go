baf24f299452ce7faf65d6b9e22fcf1190899d05
Jiten <jpalaparthi@gmail.com>
2023-07-31 20:11:08 +0530

    modify hello app

--- 1-hello/main.go
+++ 1-hello/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+import "os"
+import "time"
+
 func main() {
 	println("Hello World!")
+	fmt.Println("Hello World!", time.Now())
+	os.Exit(0) // successful exit
 }
 
 // go runtime can recognise main func as the starting point and that main func must be in main pacakge.
